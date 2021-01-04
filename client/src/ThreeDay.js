67062076a6aa34bf87c6f87fa56e37c3600bbcb9
darkquilius <andrew.c.reeves27@gmail.com>
2021-01-04 12:44:33 -0500

    humid description on threeday

--- client/src/ThreeDay.js
+++ client/src/ThreeDay.js
@@ -12,7 +12,7 @@ const ThreeDay = ({weather, temp, humidity, wind_speed, index}) => {
         <img alt="Current Weather" src={`http://openweathermap.org/img/wn/${weather[0].icon}@4x.png`} />
         <h3 className="description">{weather[0].description.toUpperCase()}</h3>
         <p>{temp.day.toString().substring(0,2)}&#176;F</p>
-        <p>{humidity}%</p>
+        <p>Humidity: {humidity}%</p>
         <p>Windspeed: {wind_speed} MPH</p>
       </Wrapper>
   )
